96ea25ad513234ecae115cb629737ea0045b6e67
Murphy <yuanzx@163.com>
2015-04-23 15:09:44 +0800

    asdf
    asdf

--- public/static/js/employer.js
+++
@@ -1,6 +0,0 @@
-$('.second-block .mobile li > a').click(function(ev) {
-  ev.preventDefault();
-  ev.stopPropagation();
-  $('.second-block .mobile li .sub-content').slideUp();
-  return $(this).parent().find('.sub-content').slideDown();
-});
