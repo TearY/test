b5b507cc21fd1687fb4826fc11c8f092cbfe0f4a
Murphy <yuanzx@163.com>
2015-06-16 10:55:17 +0800

    asfd
    asdf

--- mail.js
+++ mail.js
@@ -1,8 +1,8 @@
 var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport();
 transporter.sendMail({
-    from: '123@163.com',
-    to: 'krabs.xie@loweprofero.com',
-    subject: '准备gg',
-    text: 'lol搞起啊'
+    from: 'xijinping@163.com',
+    to: 'phoebe.sun@loweprofero.com',
+    subject: '少年，我看好你哦',
+    text: '少年，我看好你哦'
 });
